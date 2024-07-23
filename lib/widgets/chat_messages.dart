4bda3a56604d6263260ba23bf7ec9ebe416ffab4
MarkMahrous <markmahrous012@gmail.com>
2024-07-23 03:04:49 +0300

    push notifications

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,7 +1,8 @@
-import 'package:chat_app/widgets/message_bubble.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:chat_app/widgets/message_bubble.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
